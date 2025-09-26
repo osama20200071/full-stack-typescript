db87317aac2030e1a5e03cb7ed6c81c5403520ff
Osama <osamakaram118@gmail.com>
2025-09-26 18:13:06 +0300

    generate zod schemas from the openApi contract using orval

--- client/orval.config.cjs
+++ client/orval.config.cjs
@@ -3,10 +3,12 @@ module.exports = {
   'busy-bee': {
     output: {
       client: 'zod',
+      // to make the output in one big file or "split if it's large codebase"
       mode: 'single',
       target: './src/schemas.ts',
     },
     input: {
+      // we can point out an actual URL "hosted openapi  Contract"
       target: './openapi.json',
     },
   },
