70abc2acaa1fbbb39b9f097272a038ce47b3d187
Osama <osamakaram118@gmail.com>
2025-10-04 21:10:25 +0300

    adding more schemas

--- shared/schemas.ts
+++ shared/schemas.ts
@@ -1,18 +1,20 @@
 import { z } from 'zod';
 
-// export {};
-
 export const TaskSchema = z.object({
   id: z.coerce.number(),
   title: z.string(),
-  description: z.string().optional(),
+  description: z.string().nullable(),
   completed: z.coerce.boolean().default(false),
 });
 
 export const CreateTaskSchema = TaskSchema.omit({ id: true });
 export const UpdateTaskSchema = TaskSchema.omit({ id: true }).partial();
 export const TaskListSchema = z.array(TaskSchema);
+export const TaskListQuerySchema = TaskSchema.pick({ completed: true }).partial();
+export const FilterSchema = TaskSchema.pick({ completed: true }).partial();
+export const TaskIdSchema = TaskSchema.pick({ id: true });
 
 export type Task = z.infer<typeof TaskSchema>;
 export type TaskList = z.infer<typeof TaskListSchema>;
 export type PartialTask = z.infer<typeof UpdateTaskSchema>;
+export type CreateTask = z.infer<typeof CreateTaskSchema>;
